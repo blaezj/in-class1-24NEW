e7b11635f38624a2769924df798fe2b18d46b52d
blaezj <blaezjib@icloud.com>
2024-01-25 14:56:59 -0500

    finished class, pushing to main (WHICH IS EVIL) but nothing was on main anyways

--- javaProg.java
+++ javaProg.java
@@ -6,15 +6,15 @@
  **/
 import java.util.Scanner; //Scanner Class
  
-class coolCar
+abstract class coolCar
 {
 	String color = "N/A";
 	//describes car honk noise i guess
 	public abstract void carHonk();
 	
-	public void setCarColor(String color);
+	public void setCarColor(String color)
 	{
-		coolCar.color = color;
+		color = color;
 	}
 
 	public void displayCarColor()
@@ -29,7 +29,7 @@ class Truck extends coolCar
 	//truck has a annoying horn installed
 	public void carHonk()
 	{
-		System.out.println("BEEEEEEEEEEEEEEEEEEEP TRUCK BEEEEEP");
+		System.out.println("BEEEEEEEEEEEEEEEEEEEP TRUCK BEEEEEP\n");
 	}		
 }
 
@@ -38,16 +38,63 @@ class MiniCooper extends coolCar
 	//MiniCooper has a not loud horn
 	public void carHonk()
 	{
-		System.out.print("MiniCooper beep");
-
+		System.out.print("MiniCooper beep\n");
+	}
 }
 
 class Demo
 {
 	public static void main(String[] args)
 	{
+		MiniCooper coop = null;
+		Truck truck = null;
+		String response = "";
 		Scanner theRead = new Scanner(System.in);
-		Scanner.out.println("finish this");
+		System.out.println("Hello. Which car would you like: Truck or MiniCooper. Please type your response\n");
+		while (!(response.equals("truck") || response.equals("Truck") || response.equals("mini cooper") || response.equals("Mini Cooper") || response.equals("MiniCooper") || response.equals(("minicooper"))))
+		{
+			response = theRead.nextLine();
+			if(!(response.equals("truck") || response.equals("Truck") || response.equals("mini cooper") || response.equals("Mini Cooper") || response.equals("MiniCooper") || response.equals(("minicooper"))))
+
+			{
+				System.out.println("Error in responce. Please choose between Truck or MiniCooper.\n"); 
+			}
+			else
+			{
+				//create object
+				if(response.equals("truck") || response.equals("Truck"))
+				{
+					truck = new Truck();
+				}
+				else
+				{
+					coop = new MiniCooper();
+				}		
+				System.out.println("Thank you. You chose: " + response + ". Now please chose a color for your vehicle.\n");
+				String color = theRead.nextLine();
+				
+				if(response.equals("truck") ||  response.equals("Truck"))
+                                {
+                                        truck.color = color;
+                                }
+                                else
+                                {
+                                        coop.color = color;
+                                }
+				
+				System.out.println("Wow. " + color + ". Thats a great one. Here is some info about your car.\n");
+				System.out.println("Car Type: " + response +  ". Car Color: " + color + ". Car HONK: ");
+				
+                                if(response.equals("truck") ||  response.equals("Truck"))
+                                {
+                                 truck.carHonk();       
+                                }
+				else
+				{
+				 coop.carHonk();
+				}					
+			}
+		}
 
 	}
 
